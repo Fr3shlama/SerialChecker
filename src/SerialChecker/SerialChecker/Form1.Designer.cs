2cb7d20a7d7368ec8c021ce6c863464808ef0c08
Fr3shlama <waffenschmidt14@gmail.com>
2019-01-07 15:55:27 +0100

    New version bois

---
+++ src/SerialChecker/SerialChecker/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SerialChecker
+{
+    partial class SerialChecker
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SerialChecker));
+            this.serPort = new System.IO.Ports.SerialPort(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ListBox = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.count = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.isLoop = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(12, 12);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(62, 214);
+            this.ListBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // count
+            // 
+            this.count.AutoSize = true;
+            this.count.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.count.Location = new System.Drawing.Point(84, 9);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(77, 17);
+            this.count.TabIndex = 3;
+            this.count.Text = "Count: 999";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(100, 213);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(61, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Visit my site";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // isLoop
+            // 
+            this.isLoop.AutoSize = true;
+            this.isLoop.Location = new System.Drawing.Point(87, 61);
+            this.isLoop.Name = "isLoop";
+            this.isLoop.Size = new System.Drawing.Size(50, 17);
+            this.isLoop.TabIndex = 5;
+            this.isLoop.Text = "Loop";
+            this.isLoop.UseVisualStyleBackColor = true;
+            // 
+            // SerialChecker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(174, 230);
+            this.Controls.Add(this.isLoop);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.count);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SerialChecker";
+            this.Text = "SerialChecker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serPort;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckedListBox ListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label count;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox isLoop;
+    }
+}
+
---
+++ src/SerialChecker/SerialChecker/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace SerialChecker
+{
+    public partial class SerialChecker : Form
+    {
+
+        public SerialChecker()
+        {
+            InitializeComponent();    
+        }
+
+        new void Update()
+        {
+
+            var portNames = SerialPort.GetPortNames();
+            int PortCounter = 0;
+            this.ListBox.Items.Clear();
+
+            foreach (var port in portNames)
+            {
+                PortCounter++;
+                count.Text = "Count: " + Convert.ToString(PortCounter);
+
+                try
+                {
+                    serPort.PortName = port;
+                    serPort.Open();
+                    this.ListBox.Items.Add(port, false);
+                }
+                catch
+                {
+                    this.ListBox.Items.Add(port, true);
+                }
+                
+                try
+                {
+                    serPort.Close();
+                }
+                catch { }  //If a port is open, it will get closed.
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Update();
+            if (isLoop.Checked) MessageBox.Show("You just found a debug feauture!");
+        }
+   
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://lama.host");
+        }
+
+    }
+}
